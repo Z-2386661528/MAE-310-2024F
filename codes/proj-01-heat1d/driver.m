b4eaf2cf7c88eb837bb1d8698320ceac7748d5a9
Z-2386661528 <2386661528@qq.com>
2024-11-28 18:21:50 +0800

    part1

--- codes/proj-01-heat1d/driver.m
+++ codes/proj-01-heat1d/driver.m
@@ -1,14 +1,14 @@
-clear all; clc; % clean the memory and screen
+clear all; clc; clf; % clean the memory, screen, and figure
 
-% Define the external source or force and boundary data
-f = @(x) -20*x.^3; % f(x) = x
+% Problem definition
+f = @(x) -20*x.^3; % f(x) is the source
 g = 1.0;           % u    = g  at x = 1
 h = 0.0;           % -u,x = h  at x = 0
 
 % Setup the mesh
 pp   = 2;              % polynomial degree
 n_en = pp + 1;         % number of element or local nodes
-n_el = 4;              % number of elements
+n_el = 2;              % number of elements
 n_np = n_el * pp + 1;  % number of nodal points
 n_eq = n_np - 1;       % number of equations
 n_int = 10;
@@ -32,22 +32,22 @@ ID(end) = 0;
 [xi, weight] = Gauss(n_int, -1, 1);
 
 % allocate the stiffness matrix
-K = zeros(n_eq, n_eq);
+K = spalloc(n_eq, n_eq, (2*pp+1)*n_eq);
 F = zeros(n_eq, 1);
 
+% Assembly of the stiffness matrix and load vector
 for ee = 1 : n_el
-  
   k_ele = zeros(n_en, n_en); % allocate a zero element stiffness matrix
   f_ele = zeros(n_en, 1);    % allocate a zero element load vector
 
-  x_ele = x_coor(IEN(ee,:));
+  x_ele = x_coor(IEN(ee,:)); % x_ele(aa) = x_coor(A) with A = IEN(aa, ee)
 
-  for qua = 1 : n_int
-    
+  % quadrature loop
+  for qua = 1 : n_int    
     dx_dxi = 0.0;
     x_l = 0.0;
     for aa = 1 : n_en
-      x_l = x_l + x_ele(aa) * PolyShape(pp, aa, xi(qua), 0);
+      x_l    = x_l    + x_ele(aa) * PolyShape(pp, aa, xi(qua), 0);
       dx_dxi = dx_dxi + x_ele(aa) * PolyShape(pp, aa, xi(qua), 1);
     end
     dxi_dx = 1.0 / dx_dxi;
@@ -60,8 +60,7 @@ for ee = 1 : n_el
     end
   end
  
-  % check the ID(IEN(ee, aa)) and ID(IEN(ee,bb)), if they are positive
-  % put the element stiffness matrix into K
+  % Assembly of the matrix and vector based on the ID or LM data
   for aa = 1 : n_en
     P = ID(IEN(ee,aa));
     if(P > 0)
@@ -76,16 +75,138 @@ for ee = 1 : n_el
       end
     end
   end
-
-  if ee == 1
-    F(ID(IEN(ee,1))) = F(ID(IEN(ee,1))) + h;
-  end
 end
+
+% ee = 1 F = NA(0)xh
+F(ID(IEN(1,1))) = F(ID(IEN(1,1))) + h;
 
 % Solve Kd = F equation
 d_temp = K \ F;
 
 disp = [d_temp; g];
+
+% Postprocessing: visualization
+%plot(x_coor, disp, '--r','LineWidth',3);
+
+%x_sam = 0 : 0.01 : 1;
+%y_sam = x_sam.^5;
+%hold on;
+%plot(x_sam, y_sam, '-k', 'LineWidth', 3);
+
+n_sam = 20;
+xi_sam = -1 : (2/n_sam) : 1;
+
+x_sam = zeros(n_el * n_sam + 1, 1);
+y_sam = x_sam; % store the exact solution value at sampling points
+u_sam = x_sam; % store the numerical solution value at sampling pts
+
+for ee = 1 : n_el
+  x_ele = x_coor( IEN(ee, :) );
+  u_ele = disp( IEN(ee, :) );
+
+  if ee == n_el
+    n_sam_end = n_sam+1;
+  else
+    n_sam_end = n_sam;
+  end
+
+  for ll = 1 : n_sam_end
+    x_l = 0.0;
+    u_l = 0.0;
+    for aa = 1 : n_en
+      x_l = x_l + x_ele(aa) * PolyShape(pp, aa, xi_sam(ll), 0);
+      u_l = u_l + u_ele(aa) * PolyShape(pp, aa, xi_sam(ll), 0);
+    end
+
+    x_sam( (ee-1)*n_sam + ll ) = x_l;
+    u_sam( (ee-1)*n_sam + ll ) = u_l;
+    y_sam( (ee-1)*n_sam + ll ) = x_l^5;
+  end
+end
+
+
+plot(x_sam, u_sam, '-r','LineWidth',3);
+hold on;
+plot(x_sam, y_sam, '-k','LineWidth',3);
+
+for n_el = 2 : 2 : 16                             % hw4 2
+    n_np = n_el * pp + 1;                         % copy above which changed by n_el
+    n_eq = n_np - 1;
+
+    hh = 1.0 / (n_np - 1);
+    x_coor = 0 : hh : 1;
+
+    IEN = zeros(n_el , n_en);
+    for ee = 1 : n_el
+        for aa = 1 : n_en
+            IEN(ee , aa) = (ee - 1) * pp + aa;
+        end
+    end
+
+    ID = 1 : n_np;
+    ID(end) = 0;
+
+    K = spalloc(n_eq, n_eq, (2*pp+1)*n_eq);
+    F = zeros(n_eq, 1);
+
+    for ee = 1 : n_el
+        k_ele = zeros(n_en, n_en);
+        f_ele = zeros(n_en, 1);
+
+        x_ele = x_coor(IEN(ee,:));
+
+        for qua = 1 : n_int
+            dx_dxi = 0.0;
+            x_l = 0.0;
+            for aa = 1 : n_en
+                x_l = x_l + x_ele * PolyShape(pp, aa, xi(qua), 0);
+                dx_dxi = dx_dxi + x_ele * PolyShape(pp, aa, xi(qua), 1);
+            end
+            %dxi_dx = 1 / dx_dxi;
+
+            for aa = 1 : n_en
+
+
+            end
+        end
+    end
+
+
+
+
+end
+
+[t01, t01_weight] = Gauss(n_int, 0, 1);
+eL22 = 0.0;
+eH12 = 0.0;
+du_dx = @(x) 5 * x.^4;
+uu    = @(x) x.^5;
+
+for qua = 1 : n_int
+    eH12 = eH12 + t01_weight(qua) * du_dx(t01(qua)) ^ 2;
+    eL22 = eL22 + t01_weight(qua) * uu(t01(qua)) ^ 2;
+    if qua == n_int
+        eH12 = eH12 ^ 0.5;
+        eL22 = eL22 ^ 0.5;
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
