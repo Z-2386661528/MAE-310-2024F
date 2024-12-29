292418a91c65de60d2c5bb98c1b771f34feff2b7
Z-2386661528 <2386661528@qq.com>
2024-12-29 17:45:28 +0800

    some part

--- HW6/Quad.m
+++ HW6/Quad.m
@@ -1,13 +1,26 @@
-function val = Quad(aa, xi, eta)
-
-if aa == 1
-    val = 1 - xi - eta ;
-elseif aa == 2
-    val = xi;
-elseif aa == 3
-    val = eta;
-else
-    error('Error: value of a should be 1,2,3, or 4.');
+function val = Quad(aa, xi, eta,n_en)
+if n_en == 3
+    if aa == 1
+        val = 1 - xi - eta ;
+    elseif aa == 2
+        val = xi;
+    elseif aa == 3
+        val = eta;
+    else
+        error('Error: value of a should be 1,2,3, or 4.');
+    end
+elseif n_en == 4
+    if aa == 1
+        val = 0.25 * (1-xi) * (1-eta);
+    elseif aa == 2
+        val = 0.25 * (1+xi) * (1-eta);
+    elseif aa == 3
+        val = 0.25 * (1+xi) * (1+eta);
+    elseif aa == 4
+        val = 0.25 * (1-xi) * (1+eta);
+    else
+        error('Error: value of a should be 1,2,3, or 4.');
+    end
 end
 
 % EOF
--- HW6/Quad_grad.m
+++ HW6/Quad_grad.m
@@ -1,16 +1,33 @@
-function [val_xi, val_eta] = Quad_grad(aa, xi, eta)
-
-if aa == 1
-    val_xi  = -1;
-    val_eta = -1;
-elseif aa == 2
-    val_xi  =  1;
-    val_eta = 0;
-elseif aa == 3
-    val_xi  = 0;
-    val_eta = 1;
-else
-    error('Error: value of a should be 1,2,3, or 4.');
+function [val_xi, val_eta] = Quad_grad(aa, xi, eta, n_en)
+if n_en == 3
+    if aa == 1
+        val_xi  = -1;
+        val_eta = -1;
+    elseif aa == 2
+        val_xi  =  1;
+        val_eta = 0;
+    elseif aa == 3
+        val_xi  = 0;
+        val_eta = 1;
+    else
+        error('Error: value of a should be 1,2,3, or 4.');
+    end
+elseif n_en == 4
+    if aa == 1
+        val_xi  = -0.25 * (1-eta);
+        val_eta = -0.25 * (1-xi);
+    elseif aa == 2
+        val_xi  =  0.25 * (1-eta);
+        val_eta = -0.25 * (1+xi);
+    elseif aa == 3
+        val_xi  = 0.25 * (1+eta);
+        val_eta = 0.25 * (1+xi);
+    elseif aa == 4
+        val_xi  = -0.25 * (1+eta);
+        val_eta =  0.25 * (1-xi);
+    else
+        error('Error: value of a should be 1,2,3, or 4.');
+    end
 end
 
 % EOF
